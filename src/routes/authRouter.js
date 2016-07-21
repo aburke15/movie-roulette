fb9b90b79ee65ced149389f13228057e1e4a88c1
aburkex24 <aburkex24@gmail.com>
2016-07-21 15:10:45 -0600

    pushed auth and movie router

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var authRouter = express.Router();
+var express = require("express"); // pull in express to user the Router method
+var authRouter = express.Router(); // declare and instantiate the Router method
 var User = require("../models/user");
 var jwt = require("jsonwebtoken");
 var config = require("../config");
@@ -17,15 +17,19 @@ authRouter.route("/login")
                     message: "Provided username was not found!"
                 });
             } else {
+                // use bcrypt to compare the request body password with the user password
                 bcrypt.compare(req.body.password, user.password, function (err, match) {
                     if (err) {
+                        // if there is an error send status of 500
                         res.status(500).send(err);
                     } else if (!match) {
+                        // if the password does not match send 401 status and message
                         res.status(401).send({
                             success: false,
                             message: "Incorrect password!"
                         });
                     } else {
+                        // if password matches grant user a token for authentication
                         var token = jwt.sign(user._doc, config.secret);
                         res.send({
                             success: true,
@@ -40,6 +44,7 @@ authRouter.route("/login")
     });
 
 authRouter.route("/signup")
+// check to see if the username already exists
     .post(function (req, res) {
         User.findOne({
             username: req.body.username
@@ -50,13 +55,14 @@ authRouter.route("/signup")
                 message: "User name is already taken!"
             });
             else {
+                // if the user name does not exist allow the user to add a new user
                 var newUser = new User(req.body);
                 newUser.save(function (err, user) {
-                    if (err) res.status(500).send(err);
+                    if (err) res.status(500).send(err); 
                     else res.send({
                         success: true,
                         user: user,
-                        message: "Successfully created new user"
+                        message: "Successfully created a new user."
                     });
                 });
             }
--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -19,5 +19,28 @@ movieRouter.route("/")
         });
     });
 
+movieRouter.route("/:id")
+    .get(function(req, res) {
+        Movie.findOne({_id: req.params.id, user: req.user._id}, {new: true}, function (err, movie) {
+            if (err) res.status(500).send(err); 
+            else res.send(movie); 
+        });
+    })
+    .put(function(req, res) {
+        Movie.findOneAndUpdate({_id: req.params.id, user: req.user._id}, req.body, {new: true}, function(err, movie) {
+            if (err) res.status(500).send(err); 
+            else res.send(movie); 
+        });
+    })
+    .delete(function(req, res) {
+        Movie.findOneAndRemove({_id: req.params.id, user: req.user._id}, function(err, movie) {
+            if (err) res.status(500).send(err);
+            else var response = {
+                movie: movie,
+                message: "Movie was successfully deleted!"
+            }
+            res.send(response); 
+        });
+    });
 
 module.exports = movieRouter; 
