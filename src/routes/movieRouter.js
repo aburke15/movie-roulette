86e5ee1b8214c06d161b90ee0029e9ff3d17bea4
aburkex24 <aburkex24@gmail.com>
2016-07-21 00:03:34 -0600

    pushed movie model change

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -5,6 +5,19 @@ var Movie = require("../models/movie");
 movieRouter.route("/")
     .get(function(req, res) {
         Movie.find({user: req.user._id}, function(err, movies) {
-            
-        })
+            if (err) res.status(500).send(err);
+            else res.send(movies); 
+        });
     })
+    .post(function(req, res) {
+        var newMovie = Movie(req.body); 
+        newMovie.user = req.user._id;
+    
+        newMovie.save(function (err, movie) {
+            if (err) res.status(500).send(err);
+            else res.status(201).send(movie);
+        });
+    });
+
+
+module.exports = movieRouter; 
