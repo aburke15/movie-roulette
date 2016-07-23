5fc16f4386369651d426c7cd52b0c3d31cf170ab
aburkex24 <aburkex24@gmail.com>
2016-07-23 16:40:38 -0600

    pushed html pages login and signup

--- public/components/controllers/login.js
+++ public/components/controllers/login.js
@@ -1,5 +1,11 @@
 var app = angular.module("MovieApp.Auth"); 
 
-app.controller("LoginController", ["$scope" "$location", function($scope, $location) {
-    
+app.controller("LoginController", ["$scope" "$location", "UserService", function($scope, $location, UserService) {
+    $scope.login = function(user) {
+        UserService.login(user).then(function(response) {
+            $location.path("/movies");
+        }, function(response) {
+            alert("Login was unsuccessful!"); 
+        });
+    }
 }]);
