aa9d6c362e5a74f0e224b658276b54293c2b8e1e
aburkex24 <aburkex24@gmail.com>
2016-07-26 19:59:01 -0600

    pushed controller functionality signup.js

--- public/components/controllers/signup.js
+++ public/components/controllers/signup.js
@@ -1,5 +1,19 @@
-var app = angular.module("MovieApp.Auth"); 
+var app = angular.module("MovieApp.Auth");
 
-app.controller("SignupController", ["$scope", "$location", function($scope, $location) {
-    
-}]); 
+app.controller("SignupController", ["$scope", "$location", "UserService", function($scope, $location, UserService) {
+    $scope.passwordMessage = "";
+    $scope.signup = function(user) {
+        // check to see if the user password matches the repeat password
+        if (user.password !== $scope.passwordRepeat) {
+            // if passwords do not match display error message
+            $scope.passwordMessage = "Passwords do not match!";
+        } else {
+            // else send the data to the user service and navigate to the login page
+            UserService.signup(user).then(function(response) {
+                $location.path("/login");
+            }, function(response) {
+                alert("There was a problem " + response.data.message);
+            });
+        }
+    }
+}]);
