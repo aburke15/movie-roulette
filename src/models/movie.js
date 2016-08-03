8114982756db94e090a3eb645a370e8fbf99ef56
aburkex24 <aburkex24@gmail.com>
2016-08-02 23:52:00 -0600

    pushed new movie schema

--- src/models/movie.js
+++ src/models/movie.js
@@ -2,6 +2,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema; 
 
 var movieSchema = new Schema({
-    
+    title: {
+        type: String,
+        required: false
+    },
+    rating: {
+        type: Number,
+        required: false
+    }
 });
 module.exports = mongoose.model("Movie", movieSchema);
